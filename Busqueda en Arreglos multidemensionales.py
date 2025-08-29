9bb9e5c68c915ee84304558f705ea5228ee193ef
Bryan Ivan Pabon Garcia <bi.pabong@uea.edu.ec>
2025-08-28 20:35:28 -0500

    Se realizaron cambios en los valores de las matrices matrices, se uso el Bubble Sort

---
+++ Busqueda en Arreglos multidemensionales.py
@@ -0,0 +1,30 @@
+# Escribe un programa que incluya una matriz bidimensional (puede ser una matriz pequeña de 3x3) con valores numéricos.
+matriz = [
+    [1, 5, 8],
+    [9, 6, 5],
+    [3, 3, 6]
+]
+
+# Valor que estamos buscando
+valor_buscado = 1
+
+# Inicializar variables para rastrear la posición del valor
+fila_encontrada = -1
+columna_encontrada = -1
+
+# Recorrer la matriz para buscar el valor
+for fila in range(len(matriz)):
+    for columna in range(len(matriz[fila])):
+        if matriz[fila][columna] == valor_buscado:
+            fila_encontrada = fila
+            columna_encontrada = columna
+            break  # Detener la búsqueda una vez que se encuentra el valor
+    if fila_encontrada != -1:
+        break  # Salir del bucle exterior si se encuentra el valor
+
+# Verificar si se encontró el valor y mostrar la posición
+if fila_encontrada != -1:
+    print(f"Se encontró {valor_buscado} en la fila {fila_encontrada} y columna {columna_encontrada}.")
+else:
+    print(f"{valor_buscado} no se encontró en la matriz.")
+
