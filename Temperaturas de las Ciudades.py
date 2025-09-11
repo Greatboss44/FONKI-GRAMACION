16ba631f4bf6e3c342b17b20a2c85dc2924d5c20
Bryan Ivan Pabon Garcia <bi.pabong@uea.edu.ec>
2025-09-11 05:39:59 -0500

    "Función para calcular temperatura promedio por ciudad"

--- Temperaturas de las Ciudades.py
+++ Temperaturas de las Ciudades.py
@@ -3,36 +3,37 @@
 # Variables
 ciudades = ["Quito", "Guayaquil", "Ambato"]
 dias_semana = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"]
-num_semanas = 2  # 2 semanas
+num_semanas = 4  # 4 semanas
 
 
 temperaturas = [
     [  # Quito
-        [20, 21],  # Lunes (semana 1, semana 2)
-        [22, 23],  # Martes
-        [19, 20],  # Miércoles
-        [21, 22],  # Jueves
-        [18, 19],  # Viernes
-        [20, 21],  # Sábado
-        [21, 22]   # Domingo
+        [20, 21, 17, 20],  # Lunes (semana 1, semana 2, semana 3, semana 4)
+        [22, 23, 16, 18],  # Martes
+        [19, 20, 22, 19],  # Miércoles
+        [21, 22, 19, 23],  # Jueves
+        [18, 19, 20, 17],  # Viernes
+        [20, 21, 20, 19],  # Sábado
+        [21, 22, 22, 19]   # Domingo
     ],
     [  # Guayaquil
-        [28, 29],
-        [30, 31],
-        [29, 28],
-        [32, 33],
-        [31, 30],
-        [30, 29],
-        [28, 27]
+        [28, 29, 25, 28],
+        [30, 31,32 , 26],
+        [29, 28, 33, 30],
+        [32, 33, 28, 34],
+        [31, 30, 34, 30],
+        [30, 29, 34, 29],
+        [28, 27, 30, 27 ]
     ],
     [  # Ambato
-        [15, 16],
-        [17, 18],
-        [16, 17],
-        [15, 16],
-        [14, 15],
-        [15, 16],
-        [17, 18]
+        [15, 16, 13, 14],
+        [14, 15, 16, 15],
+        [17, 18, 19, 18]    ,
+        [16, 17, 18, 16],
+        [15, 16, 18, 15],
+        [14, 15, 19, 15],
+        [17, 18, 19, 18],
+
     ]
 ]
 
